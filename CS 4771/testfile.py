8184ee274ee73424c3091dba65c72732416b881f
cart7982 <tcart00990@gmail.com>
2025-09-11 14:14:04 -0700

    w/e

---
+++ CS 4771/testfile.py
@@ -0,0 +1,29 @@
+print('Alex')
+
+class s1(object):
+    def __init__(self)
+        
+
+s1 = "'"
+s2 = 'C:\\documents\\'
+
+
+print(s2.spint('\\'))
+
+
+s2*10  #repeats it ten times
+
+s1.replace('This, 'That')
+
+#string type is a class
+
+s1.upper() #upper case, lower case, etc.
+
+#check for tools to capitalize, he skipped over them
+
+'    a    word    '.rstrip()
+#.strip will strip away whitespace on left/right
+#.lstrip will remove whitespace from the left, .rstrip
+
+#\\ escape character
+
